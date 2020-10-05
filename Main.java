aa3355a6a068013d990fa8ea4d8e3141bd26917e
Veronika Stanovská <veronika.stanovska@kosickaakademia.sk>
2020-10-05 12:27:36 +0200

    I add program that reverse number and says if the number is Palindrome

---
+++ Main.java
@@ -0,0 +1,36 @@
+package sk.kosickaakademia.stanovska.projectmath.cyklus;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sem zadaj číslo: ");
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int x2=x;
+        int reversed= 0;
+        while (x2>0){
+            reversed= reversed + (x2%10);
+            reversed = reversed * 10;
+            x2= x2/10;
+        }
+        reversed= reversed/10;
+        if (reversed==x)
+            System.out.println("čislo je palindróm");
+        else
+            System.out.println("čislo nie je palindróm");
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
